d5f639c55c04e459397da4e89aa511405df743a5
Pratham Sharma <sharmapratham1951@gmail.com>
2023-11-22 01:32:09 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -5,6 +5,13 @@ terraform {
       version = "=3.78.0"
     }
   }
+ 
+backend "azurerm" {
+    resource_group_name  = "pratham-backend"
+    storage_account_name = "pratham1951"
+    container_name       = "tfstate"
+    key                  = "dev.terraform.tfstate"
+  }
 }
 provider "azurerm" {
   features {}
